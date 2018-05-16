cd503e0c44d0e1ac2730ec13511b555cb2eccc0f
Duncan <marjoribanks.d@gmail.com>
2018-05-16 13:27:54 +0100

    card type set up and enums set up to restrict card editing

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest {
+    Card card;
+
+    @Before
+    public void before(){
+        card = new Card(SuitType.CLUBS, RankType.JACK);
+    }
+
+    @Test
+    public void canGetSuit(){
+        assertEquals(SuitType.CLUBS, card.getSuit());
+    }
+}
